11fa0a76bac5e430d5f4413eff84ae2723cd737f
brandon <brandonliu5658@gmail.com>
2025-02-12 14:28:11 -0500

    starting lcd code

--- CubeIDE/Core/Src/main.c
+++ CubeIDE/Core/Src/main.c
@@ -165,7 +165,7 @@ static void MX_TIM7_Init(void)
   htim7.Instance = TIM7;
   htim7.Init.Prescaler = 7999;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 1;
+  htim7.Init.Period = 4;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
   {
