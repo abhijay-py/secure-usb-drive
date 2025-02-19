1e9b8b0abcf0bec878b44a6c85ad144bede06b1e
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2025-02-19 14:47:56 -0500

    wrote all fingerprint sensor TX code

--- CubeIDE/Core/Inc/fingerprint_sensor.h
+++ CubeIDE/Core/Inc/fingerprint_sensor.h
@@ -9,5 +9,9 @@
 #define INC_FINGERPRINT_SENSOR_H_
 
 void get_img();
+void gen_char(const uint8_t buffer_id);
+void search(const uint8_t buffer_id, const uint16_t start_page, const uint16_t num_pages);
+void reg_model();
+void store(const uint8_t buffer_id, const uint16_t page_id);
 
 #endif /* INC_FINGERPRINT_SENSOR_H_ */
--- CubeIDE/Core/Src/fingerprint_sensor.c
+++ CubeIDE/Core/Src/fingerprint_sensor.c
@@ -9,14 +9,18 @@
 #include "main.h"
 
 #define MAX_SIZE 12 // TODO: Change later
-uint8_t tx_buff[MAX_SIZE];
 
+// Sends a command to the fingerprint sensor with Address = 0xFFFFFFFF,
+// 0x01 package identifier, package contents = data.
+// data_length corresponds to the length of data, not the package length.
 static void send_command(const uint8_t data[], const uint16_t data_length) {
+	uint8_t tx_buff[MAX_SIZE];
+
 	// Header
 	tx_buff[0] = 0xEF;
 	tx_buff[1] = 0x01;
 
-	// Adder
+	// Address
 	for (int i = 0; i < 4; i++) {
 		tx_buff[i+2] = 0xFF;
 	}
@@ -25,7 +29,7 @@ static void send_command(const uint8_t data[], const uint16_t data_length) {
 
 	// Package identifier
 	tx_buff[6] = 0x01;
-	sum += pid;
+	sum += 1;
 
 	// Package length
 	uint16_t length = data_length + 2;
@@ -46,7 +50,44 @@ static void send_command(const uint8_t data[], const uint16_t data_length) {
 	HAL_UART_Transmit(&huart5, tx_buff, data_length + 11, 1000);
 }
 
+// See the GetImg command in the datasheet
 void get_img() {
-	send_command({0x01}, 1);
+	const uint8_t data[] = {0x01};
+	send_command(data, 1);
 }
 
+// See the GenChar command in the datasheet
+void gen_char(const uint8_t buffer_id) {
+	const uint8_t data[] = {0x02, buffer_id};
+	send_command(data, 2);
+}
+
+// See the Search command in the datasheet
+void search(const uint8_t buffer_id, const uint16_t start_page, const uint16_t num_pages) {
+	const uint8_t data[] = {
+		0x04,
+		buffer_id,
+		(uint8_t)(start_page >> 8),
+		(uint8_t)start_page,
+		(uint8_t)(num_pages >> 8),
+		(uint8_t)num_pages
+	};
+	send_command(data, 6);
+}
+
+// See the RegModel command in the datasheet
+void reg_model() {
+	const uint8_t data[] = {0x05};
+	send_command(data, 1);
+}
+
+// See the Store command in the datasheet
+void store(const uint8_t buffer_id, const uint16_t page_id) {
+	const uint8_t data[] = {
+		0x06,
+		buffer_id,
+		(uint8_t)(page_id >> 8),
+		(uint8_t)page_id
+	};
+	send_command(data, 4);
+}
