893f4930e26d5da833211b1cde603abf96983cc5
brandon <brandonliu5658@gmail.com>
2025-02-24 16:50:29 -0500

    improved code quality of lcd.c

--- CubeIDE/Core/Inc/lcd.h
+++ CubeIDE/Core/Inc/lcd.h
@@ -10,6 +10,8 @@
 
 #include "stm32f4xx_hal_spi.h"
 
+#define HOME 0x46
+
 void lcd_welcome(SPI_HandleTypeDef *hspi2);
 void lcd_clear(SPI_HandleTypeDef *hspi2);
 void lcd_on(SPI_HandleTypeDef *hspi2);
@@ -19,7 +21,7 @@ void lcd_display_first_row(SPI_HandleTypeDef *hspi2, uint8_t *string);
 void lcd_display_second_row(SPI_HandleTypeDef *hspi2, uint8_t *string);
 void periodic_send(SPI_HandleTypeDef *hspi2);
 
-void send_string(SPI_HandleTypeDef *hspi2, uint8_t *string);
-void send_byte(SPI_HandleTypeDef *hspi2, const uint8_t *byte);
+//void send_string(SPI_HandleTypeDef *hspi2, uint8_t *string);
+void send_bytes(SPI_HandleTypeDef *hspi2, const uint8_t *tx_buffer, size_t size);
 
 #endif /* INC_LCD_H_ */
--- CubeIDE/Core/Src/lcd.c
+++ CubeIDE/Core/Src/lcd.c
@@ -9,22 +9,18 @@
 #include "stm32f4xx_hal_spi.h"
 
 const uint8_t FN_SET_CMD = 0x38;
-const uint8_t LCD_ON_CMD = 0x41; // 0x0c for 362 LCD
-const uint8_t LCD_OFF_CMD = 0x42; // 0x08 for 362 LCD
-const uint8_t LCD_CLR_CMD = 0x51; // 0x01 for 362 LCD
+const uint8_t LCD_ON_CMD = 0x41;
+const uint8_t LCD_OFF_CMD = 0x42;
+const uint8_t LCD_CLR_CMD = 0x51;
 const uint8_t LCD_ENTRY_CMD = 0x06;
-const uint8_t LCD_HOME_CMD = 0x46; // 0x02 for 362 LCD
-const uint8_t LCD_2ND_ROW_CMD = 0xc0;
-const uint8_t LCD_DATA_PAIR_CMD = 0x02; // this must be the 3rd set of 4 bits
+const uint8_t LCD_HOME_CMD = 0x46;
+const uint8_t LCD_CURS_LOC_CMD = 0x45;
 const uint8_t LCD_PREFIX = 0xFE;
-const uint8_t LCD_CHAR_CMD = 0x41;
-
-const uint8_t LCD_CURSOR_ON_BLINK = 0x0F;
 
 void lcd_display_first_row(SPI_HandleTypeDef *hspi2, uint8_t *string) {
-	HAL_SPI_Transmit(hspi2, &LCD_ON_CMD, 1, 1000); //Sending in normal mode
-	HAL_Delay(100);
-	send_string(hspi2, string);
+//	HAL_SPI_Transmit(hspi2, &LCD_ON_CMD, 1, 1000); //Sending in normal mode
+//	HAL_Delay(100);
+//	send_string(hspi2, string);
 }
 //
 //void lcd_display_second_row(SPI_HandleTypeDef *hspi2, uint8_t *string) {
@@ -34,80 +30,100 @@ void lcd_display_first_row(SPI_HandleTypeDef *hspi2, uint8_t *string) {
 //}
 
 void lcd_cursor_on(SPI_HandleTypeDef *hspi2) {
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(hspi2, &LCD_CURSOR_ON_BLINK, 1, 1000); //Sending in normal mode
-//	HAL_Delay(100);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	HAL_SPI_Transmit(hspi2, &LCD_CURSOR_ON_BLINK, 1, 1000); //Sending in normal mode
+////	HAL_Delay(100);
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+}
+
+// note that the hex location ranges from column 0x00 to 0x0F for row 1
+// note that the hex location ranges from column 0x40 to 0x4F for row 2
+void lcd_cursor_location(SPI_HandleTypeDef *hspi2, const uint8_t hex_location) {
+	HAL_Delay(1000);
+	if (hex_location == HOME) {
+		// call home command
+	}
+	else {
+		const uint8_t tx_buffer[] = {LCD_PREFIX, LCD_CURS_LOC_CMD, hex_location};
+
+	}
 }
 
 void lcd_welcome(SPI_HandleTypeDef *hspi2) {
-	HAL_Delay(1000);
+//	HAL_Delay(1000);
 	const uint8_t tx_buffer[] = {'w','e','l','c','o','m','e',' ',
 								'2',' ',
 								'e','c','e','4','7','7'};
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	for (int i = 0; i < sizeof(tx_buffer); i++) {
+//		HAL_SPI_Transmit(hspi2, tx_buffer + i, 1, 1000);
+//		while(hspi2 -> State != HAL_SPI_STATE_READY);
+//		HAL_Delay(100);
+//	}
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+	send_bytes(hspi2, tx_buffer, sizeof(tx_buffer));
+}
+
+void lcd_clear(SPI_HandleTypeDef *hspi2) {
+//	HAL_Delay(1000);
+	const uint8_t tx_buffer[] = {LCD_PREFIX, LCD_CLR_CMD};
+
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	HAL_SPI_Transmit(hspi2, tx_buffer, 1, 1000);
+//	while(hspi2 -> State != HAL_SPI_STATE_READY);
+//
+////	const uint8_t tx_buffer2[] = {LCD_CLR_CMD};
+//
+////	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	HAL_SPI_Transmit(hspi2, tx_buffer + 1, 1, 1000);
+//	while(hspi2 -> State != HAL_SPI_STATE_READY);
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+	send_bytes(hspi2, tx_buffer, sizeof(tx_buffer));
+}
+
+void lcd_on(SPI_HandleTypeDef *hspi2) {
+//	HAL_Delay(1000);
+	const uint8_t tx_buffer[] = {LCD_PREFIX, LCD_ON_CMD};
+
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	HAL_SPI_Transmit(hspi2, tx_buffer, 1, 1000);
+//	while(hspi2 -> State != HAL_SPI_STATE_READY);
+//
+////	const uint8_t tx_buffer2[] = {LCD_CLR_CMD};
+//
+////	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	HAL_SPI_Transmit(hspi2, tx_buffer + 1, 1, 1000);
+//	while(hspi2 -> State != HAL_SPI_STATE_READY);
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+	send_bytes(hspi2, tx_buffer, sizeof(tx_buffer));
+}
+
+void lcd_off(SPI_HandleTypeDef *hspi2) {
+//	HAL_Delay(1000);
+	const uint8_t tx_buffer[] = {LCD_PREFIX, LCD_OFF_CMD};
+
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	HAL_SPI_Transmit(hspi2, tx_buffer, 1, 1000);
+//	while(hspi2 -> State != HAL_SPI_STATE_READY);
+//
+////	const uint8_t tx_buffer2[] = {LCD_CLR_CMD};
+//
+////	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+//	HAL_SPI_Transmit(hspi2, tx_buffer + 1, 1, 1000);
+//	while(hspi2 -> State != HAL_SPI_STATE_READY);
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+	send_bytes(hspi2, tx_buffer, sizeof(tx_buffer));
+}
+
+void send_bytes(SPI_HandleTypeDef *hspi2, const uint8_t *tx_buffer, size_t size) {
+	HAL_Delay(1000);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	for (int i = 0; i < sizeof(tx_buffer); i++) {
+	for (int i = 0; i < size; i++) {
 		HAL_SPI_Transmit(hspi2, tx_buffer + i, 1, 1000);
 		while(hspi2 -> State != HAL_SPI_STATE_READY);
 		HAL_Delay(100);
 	}
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
-}
-
-void lcd_clear(SPI_HandleTypeDef *hspi2) {
-	HAL_Delay(1000);
-	const uint8_t tx_buffer[] = {LCD_PREFIX, LCD_CLR_CMD};
-
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(hspi2, tx_buffer, 1, 1000);
-	while(hspi2 -> State != HAL_SPI_STATE_READY);
-
-//	const uint8_t tx_buffer2[] = {LCD_CLR_CMD};
-
-//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(hspi2, tx_buffer + 1, 1, 1000);
-	while(hspi2 -> State != HAL_SPI_STATE_READY);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
-}
-
-void lcd_on(SPI_HandleTypeDef *hspi2) {
-	HAL_Delay(1000);
-	const uint8_t tx_buffer[] = {LCD_PREFIX, LCD_ON_CMD};
-
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(hspi2, tx_buffer, 1, 1000);
-	while(hspi2 -> State != HAL_SPI_STATE_READY);
-
-//	const uint8_t tx_buffer2[] = {LCD_CLR_CMD};
-
-//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(hspi2, tx_buffer + 1, 1, 1000);
-	while(hspi2 -> State != HAL_SPI_STATE_READY);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
-}
-
-void lcd_off(SPI_HandleTypeDef *hspi2) {
-	HAL_Delay(1000);
-	const uint8_t tx_buffer[] = {LCD_PREFIX, LCD_OFF_CMD};
-
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(hspi2, tx_buffer, 1, 1000);
-	while(hspi2 -> State != HAL_SPI_STATE_READY);
-
-//	const uint8_t tx_buffer2[] = {LCD_CLR_CMD};
-
-//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(hspi2, tx_buffer + 1, 1, 1000);
-	while(hspi2 -> State != HAL_SPI_STATE_READY);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
-}
-
-void send_string(SPI_HandleTypeDef *hspi2, uint8_t *string) {
-	uint8_t data[2] = {LCD_DATA_PAIR_CMD, *string};
-//	for (int i = 0; string[i] != '\0'; i++) {
-		HAL_SPI_Transmit(hspi2, data, 2, 1000); // Data to be sent in 8 bits
-		HAL_Delay(100);
-//	}
 }
 
 void periodic_send(SPI_HandleTypeDef *hspi2) {
